0303427b94edfae4cbda60a96b27b776cdff9b15
ShikharJoshi98 <shikharjoshi89@gmail.com>
2024-09-27 21:18:55 +0530

    home.tsx updated

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,15 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import { BallManager } from "../game/classes/BallManager";
 import { WIDTH } from "../game/constants";
 import { pad } from "../game/padding";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Simulate } from "../components/Simulate";
 import { Quotes, FoundIssue } from "../components";
 
 export function Home() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const canvasRef = useRef<any>();
-  let [outputs, setOutputs] = useState<{ [key: number]: number[] }>({
+  let [_, setOutputs] = useState<{ [key: number]: number[] }>({
     0: [],
     1: [],
     2: [],
